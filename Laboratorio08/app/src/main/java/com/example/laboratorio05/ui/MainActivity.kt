81e2c7bf799a6cbdd355c8f62dd6d416d5a4bcce
noealas53 <00262020@uca.edu.sv>
2023-05-12 17:01:47 -0600

    add-lab08

---
+++ Laboratorio08/app/src/main/java/com/example/laboratorio05/ui/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.laboratorio05.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.laboratorio05.R
+import com.example.laboratorio05.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.root
+    }
+
+
+}
